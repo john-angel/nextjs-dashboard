846310c78ae92d94b1847988ee311906782bad73
John Angel <j_angeldiaz@hotmail.com>
2024-07-03 14:55:28 -0500

    feat: chapter 12 - Mutating Data

---
+++ app/lib/actions.ts
@@ -0,0 +1,65 @@
+'use server';
+import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+import { stat } from 'fs';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(['pending', 'paid']),
+    date: z.string(),
+  });
+   
+  const ValidateCreateInvoiceTypes = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData){
+    
+    const { customerId, amount, status} = ValidateCreateInvoiceTypes.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+    //It's a good practice to store monetary values in cents in your database to eliminate JavaScript floating-point errors
+    //and ensure greater accuracy.
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
+
+    console.log(`Create invoice for customer ${ customerId }. Amount: ${ amount }. Status: ${ status }`);
+
+    await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+}
+
+// Use Zod to update the expected types
+const ValidateUpdateInvoiceTypes = FormSchema.omit({ id: true, date: true });
+ 
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = ValidateUpdateInvoiceTypes.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
+  }
